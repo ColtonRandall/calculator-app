db2b0d0fb0c125a6b8fbb60ed8641e04be35ab87
Colton Randall <colton.randall@xero.com>
2022-06-06 16:38:52 +1200

    added useRedcuer logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
+import { useReducer } from "react";
+import DigitButton from "./DigitButton";
 import "./styles.css";
 
+// Global variables
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EQUALS: "equals",
+};
+
+// Break action into `type` and `payload`
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        // spread out current state
+        ...state,
+        // replace it & add to end
+        // Account for if currentOperand is null ("")
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      };
+    default:
+      return null;
+  }
+}
+
 function App() {
+  // const [state, dispatch] = useReducer(reducer, initialArg, init);
+
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+
   return (
     <div className="calculator-grid">
-      <dv className="output">
-        <div className="previous-operand">123</div>
-        <div className="current-operand">34587</div>
-      </dv>
+      <div className="output">
+        <div className="previous-operand">
+          {previousOperand} {operation}
+        </div>
+        <div className="current-operand">{currentOperand}</div>
+      </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      <button>÷</button>
+      <DigitButton digit="÷" dispatch={dispatch} />
       <button>1</button>
       <button>2</button>
       <button>3</button>

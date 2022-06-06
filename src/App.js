cd5ff3a37b9d98299faf371b248f4eb1bcb8853a
Colton Randall <colton.randall@xero.com>
2022-06-06 15:58:04 +1200

    updated CSS

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <dv className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">123</div>
+        <div className="current-operand">34587</div>
       </dv>
       <button className="span-two">AC</button>
       <button>DEL</button>
